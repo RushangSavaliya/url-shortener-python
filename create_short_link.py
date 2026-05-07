ac685b5e25a20f32265c828836628b6b191774d0
RushangSavaliya <rushangsavaliya19@gmail.com>
2026-05-08 04:15:46 +0530

    validating url

--- create_short_link.py
+++ create_short_link.py
@@ -2,9 +2,21 @@ import sqlite3
 import sys
 import string
 import random
+from urllib.parse import urlparse
 
 if len(sys.argv) < 2:
     print("Usage: python main.py <url>")
+    sys.exit()
+
+url = sys.argv[1]
+
+parsed = urlparse(url)
+
+if (
+    parsed.scheme not in ("http", "https")
+    or not parsed.netloc
+):
+    print("Invalid URL")
     sys.exit()
 
 characters = string.ascii_lowercase + string.digits
@@ -19,19 +31,19 @@ cur = con.cursor()
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS pair(
-    original TEXT ,
+    original TEXT,
     shorted TEXT UNIQUE
 )
 """)
-
-url = sys.argv[1]
 
 data = (url, random_string)
 
 try:
     cur.execute("INSERT INTO pair VALUES (?, ?)", data)
     con.commit()
-    print("Short URL: http://localhost/"+random_string)
+    print("Short URL: http://localhost/" + random_string)
 
 except sqlite3.IntegrityError:
     print("Duplicate detected")
+
+con.close()
